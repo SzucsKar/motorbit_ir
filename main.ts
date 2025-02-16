d5ee0f13828dd33362069d083ecf4aeed0642a14
SzucsKar <67927917+SzucsKar@users.noreply.github.com>
2025-02-16 21:07:02 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,65 @@
+IrRemote.onPressEvent(RemoteButton.NUM9, function () {
+    basic.showString("9")
+})
+IrRemote.onPressEvent(RemoteButton.D, function () {
+    basic.showString("d")
+})
+IrRemote.onPressEvent(RemoteButton.Ok, function () {
+    basic.showString("ok")
+})
+IrRemote.onPressEvent(RemoteButton.NUM0, function () {
+    basic.showString("0")
+})
+IrRemote.onPressEvent(RemoteButton.NUM7, function () {
+    basic.showString("7")
+})
+IrRemote.onPressEvent(RemoteButton.NUM8, function () {
+    basic.showString("8")
+})
+IrRemote.onPressEvent(RemoteButton.EQ, function () {
+    basic.showString("menü")
+})
+IrRemote.onPressEvent(RemoteButton.NUM3, function () {
+    basic.showString("3")
+})
+IrRemote.onPressEvent(RemoteButton.NUM1, function () {
+    basic.showString("1")
+})
+IrRemote.onPressEvent(RemoteButton.UP, function () {
+    basic.showString("up")
+})
+IrRemote.onPressEvent(RemoteButton.A, function () {
+    basic.showString("a")
+})
+IrRemote.onPressEvent(RemoteButton.NUM6, function () {
+    basic.showString("6")
+})
+IrRemote.onPressEvent(RemoteButton.Right, function () {
+    basic.showString(">")
+})
+IrRemote.onPressEvent(RemoteButton.NUM5, function () {
+    basic.showString("5")
+})
+IrRemote.onPressEvent(RemoteButton.NUM2, function () {
+    basic.showString("2")
+})
+IrRemote.onPressEvent(RemoteButton.Left, function () {
+    basic.showString("<")
+})
+IrRemote.onPressEvent(RemoteButton.B, function () {
+    basic.showString("b")
+})
+IrRemote.onPressEvent(RemoteButton.NUM4, function () {
+    basic.showString("4")
+})
+IrRemote.onPressEvent(RemoteButton.Down, function () {
+    basic.showString("v")
+})
+IrRemote.onPressEvent(RemoteButton.Add, function () {
+    basic.showString("+")
+})
+IrRemote.onPressEvent(RemoteButton.C, function () {
+    basic.showString("c")
+})
+basic.showIcon(IconNames.Confused)
+IrRemote.init(Pins.P5)
